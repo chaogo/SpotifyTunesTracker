72498e9b7b95e4fd9c8492a55c718f77b7aa2c86
Chao <chaotanggo@gmail.com>
2024-01-21 00:09:29 +0100

    feat: load the transformed data into sqlite database

--- etl.py
+++ etl.py
@@ -1,10 +1,13 @@
 import datetime
+import sqlite3
 import pandas as pd
 import requests
 import urllib
+import sqlalchemy
 
 
 URL_RECENTLY_PLAYED = 'https://api.spotify.com/v1/me/player/recently-played'
+DATABASE= "listening_history.sqlite"
 
 def extract_transform_load(access_token):
   # extract all songs listened for the last 24 hours
@@ -51,5 +54,26 @@ def extract_transform_load(access_token):
   # null check
   if tune_df.isnull().values.any():
       raise Exception("Null values found")
+  
+  # Load the transformed data into sqlite database
+
+  engine = sqlalchemy.create_engine(f'sqlite:///{DATABASE}')
+  conn = sqlite3.connect(DATABASE)
+  cursor = conn.cursor()
+
+  sql_query = """
+    CREATE TABLE IF NOT EXISTS my_played_tunes(
+        tune_name VARCHAR(200),
+        artist_name VARCHAR(200),
+        played_at VARCHAR(200),
+        CONSTRAINT primary_key_constraint PRIMARY KEY (played_at)
+    )
+  """
+
+  cursor.execute(sql_query)
+  tune_df.to_sql(name='my_played_tunes', con=engine, index=False, if_exists='append')
+
+  conn.close()
+  print('Load listening history yesterday to the database successfully')
   
   return data
